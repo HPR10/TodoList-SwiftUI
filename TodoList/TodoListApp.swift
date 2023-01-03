4403689256b19d3f064b9cb0aa34bd1fb9033c7a
Hugo Pinheiro <hugo_hp10@hotmail.com>
2023-01-02 21:16:29 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by HP on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
